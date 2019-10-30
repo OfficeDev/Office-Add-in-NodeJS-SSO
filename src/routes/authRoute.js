6b8764d520c1fda9f362668f199ec42d1d09f9c9
Ricky Kirkham <rickki@microsoft.com>
2019-10-30 14:40:09 -0700

    error handling around call to AAD token endpoint

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -32,19 +32,33 @@ router.get('/', async function(req, res, next) {
     const tenant = 'common';
     const tokenURLSegment = 'oauth2/v2.0/token';
 
-    const tokenResponse = await fetch(`${stsDomain}/${tenant}/${tokenURLSegment}`, {
-      method: 'POST',
-      body: form(formParams),
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-    const json = await tokenResponse.json();
+    try {
+      const tokenResponse = await fetch(`${stsDomain}/${tenant}/${tokenURLSegment}`, {
+        method: 'POST',
+        body: form(formParams),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      });
+      const json = await tokenResponse.json();
     
-    res.send(json);
-
+      res.send(json);
+    }
+    catch(error) {
+      res.status(500).send(error);
+    }
   }
+
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 });
 
 module.exports = router;
